677c8eeec6b66cbb9858ba2350830ed5ad4baf47
0131ryu <131ryuji@gmail.com>
2022-07-04 23:30:08 +0900

    렛츠기릿 자바스크립트1

---
+++ JS/section/sec2/test.js
@@ -0,0 +1,22 @@
+console.log(
+  "문자열이 긴 경우에는 " + "문자열을 " + "나눈 뒤 " + "다시 합칩니다."
+);
+
+const num1 = 5e4; //5*10000 = 50000
+const num2 = 5e4; //5*10000 = 50000
+const num3 = 5e-3; //5*(1/1000) = 0.005
+const num4 = 0b11; //3
+const num5 = 0o15; //13
+
+const changeNum = parseInt(111, 2); //1*2^2 + 2* 1 + 1= 7
+const thisNum = typeof NaN; //number
+
+console.log(3 ** (2 + 1)); //27
+console.log((0.3 * 10 - 0.1 * 10) / 10); //0.2
+
+//false 종류
+//false, 0, ''(빈 문자열), NaN, undefined, null
+
+console.log((5 + 4) * 3); //27
+
+//null의 타입 object(실수지만 계속 사용)
