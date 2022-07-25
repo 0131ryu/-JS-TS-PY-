105cc5b2c142866ade9177df18551b34e45c855a
0131ryu <131ryuji@gmail.com>
2022-07-25 18:39:59 +0900

    일정관리 jwt 회원가입, 로그인 API

--- Project/src/router/indexRouter.js
+++ Project/src/router/indexRouter.js
@@ -7,4 +7,20 @@ exports.indexRouter = function (app) {
   app.get("/user/:userIdx/todos", indexController.readTodo); // //read
   app.patch("/todos", indexController.updateTodo);
   app.delete("/user/:userIdx/todo/:todoIdx", indexController.deleteTodo); //delete
+
+  app.get(
+    "/dummy",
+    function (req, res, next) {
+      console.log(1);
+      next(); //다음 함수를 불러주는 역할
+    },
+    function (req, res, next) {
+      console.log(2);
+      //next();
+    },
+    function (req, res, next) {
+      console.log(3);
+      return;
+    }
+  );
 };
