6e755ed557b08428fb1e18fe1b6fffc4db6e0091
0131ryu <131ryuji@gmail.com>
2022-07-28 22:52:47 +0900

    파일 정리

--- Project/js/database.js
+++ Project/js/database.js
@@ -1,9 +1,4 @@
 const mysql = require("mysql2/promise");
+const { databaseSecret } = require("./secret");
 
-exports.pool = mysql.createPool({
-  host: "www.seongong.site",
-  user: "dummy",
-  port: "3306",
-  password: "12341234",
-  database: "myTodoDB",
-});
+exports.pool = mysql.createPool(databaseSecret);
--- Project/js/secret.js
+++ Project/js/secret.js
@@ -1,1 +1,8 @@
 exports.jwtSecret = "jwt-secret-8520";
+exports.databaseSecret = {
+  host: "www.seongong.site",
+  user: "dummy",
+  port: "3306",
+  password: "12341234",
+  database: "myTodoDB",
+};
