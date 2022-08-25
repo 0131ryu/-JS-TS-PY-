c249794d077afe594898175bb991f9eb88e9dfb3
0131ryu <131ryuji@gmail.com>
2022-08-25 15:46:21 +0900

    익스프레시 웹 서버 만들기 2

--- React/section5/app.js
+++ React/section5/app.js
@@ -1,15 +1,82 @@
 const express = require("express");
-const path = require("path");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const path = require("path");
+const multer = require("multer");
+const fs = require("fs");
+const dotenv = require("dotenv");
+const nunjucks = require("nunjucks");
 
+dotenv.config();
 const app = express();
+const indexRouter = require("./routes/index");
+const userRouter = require("./routes/user");
 
 app.set("port", process.env.PORT || 3000);
+app, set("view engine", "html");
+
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
+});
 
 app.use(morgan("dev"));
-app.use("/", express.static(path.join(__dirname, "public")));
-app.use(cookieParser());
+app.use("/", indexRouter);
+app.use("/user", userRouter);
+
+try {
+  fs.readdirSync("upload");
+} catch (error) {
+  console.error("uploads 폴더가 없어 uploads 파일을 생성합니다.");
+  fs.mkdirSync("uploads");
+}
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, "uploads/");
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+app.get("/upload", (req, res) => {
+  res.sendFile(path.join(__dirname, "multipart.html"));
+});
+
+app.post(
+  "/upload",
+  upload.fields({ name: "image1" }, { name: "image2" }, { name: "image3" }),
+  (req, res) => {
+    console.log(req.file);
+    res.send("ok");
+  }
+);
+
+app.use("/", (req, res, next) => {
+  if (req.session.id) {
+    express.static(path.join(__dirname, "public"));
+    req, res, next; //함수명이나 다름 없음, (req, res, next)를 넣어 함수를 호출
+  } else {
+    next();
+  }
+});
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUnitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+    },
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -22,7 +89,7 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  console.err(err);
+  console.error(err);
   res.status(200).send("에러 발생 근데 이유는 모름!");
 });
 
