be510fd7b1895614dd9bc47f58816e127c14157a
0131ryu <131ryuji@gmail.com>
2022-08-26 23:25:32 +0900

    사진 올리기, 팔로잉&팔로워 구현하기

--- React/section7-sns/routes/page.js
+++ React/section7-sns/routes/page.js
@@ -1,12 +1,14 @@
 const express = require("express");
-
+const { Post, User } = require("../models");
 const router = express.Router();
 
 router.use((req, res, next) => {
-  res.locals.user = null;
-  res.locals.followerCount = 0;
-  res.locals.followingCount = 0;
-  res.locals.followerIdList = [];
+  res.locals.user = req.user;
+  res.locals.followerCount = req.user ? req.user.Followers.length : 0;
+  res.locals.followingCount = req.user ? req.user.Followings.length : 0;
+  res.locals.followerIdList = req.user
+    ? req.user.Followings.map((f) => f.id)
+    : [];
   next();
 });
 
@@ -18,13 +20,22 @@ router.get("/join", (req, res) => {
   res.render("join", { title: "회원가입 - NodeBird" });
 });
 
-router.get("/", (req, res, next) => {
-  const twits = [];
-  res.render("main", {
-    title: "NodeBird",
-    twits,
-    user: req.user,
-  });
+router.get("/", async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      include: {
+        model: User,
+        attributes: ["id", "nick"],
+      },
+    });
+    res.render("main", {
+      title: "NodeBird",
+      twits: posts,
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 module.exports = router;
