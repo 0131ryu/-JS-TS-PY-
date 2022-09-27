2878123a2d7ae19b62ccddcb1be7663377c45120
0131ryu <131ryuji@gmail.com>
2022-09-27 17:59:28 +0900

    숫자야구 - class, hooks

---
+++ React-game/section3/client.jsx
@@ -0,0 +1,6 @@
+const React = require("react");
+import { createRoot } from "react-dom/client";
+// const NumberBaseball = require("./NumberBaseballClass");
+import NumberBaseball from "./NumberBaseball";
+
+createRoot(document.getElementById("root")).render(<NumberBaseball />);
