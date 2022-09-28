af5c3d244b1ace7c6ff6417623da026e8ee8e066
0131ryu <131ryuji@gmail.com>
2022-09-28 22:44:55 +0900

    가위바위보 - react

---
+++ React-game/section5/client.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+// import RSPClass from "./RSPClass";
+import RSP from "./RSP";
+
+createRoot(document.getElementById("root")).render(<RSP />);
