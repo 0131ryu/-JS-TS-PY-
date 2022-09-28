6efaeec9d588d00f86177743cce1778e5c207977
0131ryu <131ryuji@gmail.com>
2022-09-28 16:37:59 +0900

    반응시간 - react(class, hooks)

---
+++ React-game/section4/client.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+// import ResponseCheckClass from "./ResponseCheckClass";
+import ResponseCheck from "./ResponseCheck";
+
+createRoot(document.getElementById("root")).render(<ResponseCheck />);
