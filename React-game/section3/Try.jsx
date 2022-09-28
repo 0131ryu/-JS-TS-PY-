ddd4751ffd16d1591bd12556d2561e74c474188c
0131ryu <131ryuji@gmail.com>
2022-09-28 14:00:09 +0900

    props 설명

--- React-game/section3/Try.jsx
+++ React-game/section3/Try.jsx
@@ -11,3 +11,21 @@ const Try = memo(({ tryInfo }) => {
 Try.displayName = "Try";
 
 export default Try;
+
+// import React, { memo, useState } from "react";
+
+// const Try = memo(({ tryInfo }) => {
+//   const [result, setResult] = useState(tryInfo.result);
+//   const onClick = () => {
+//     setResult("1");
+//   };
+//   return (
+//     <li>
+//       <div>{tryInfo.try}</div>
+//       <div onClick={onClick}>{tryInfo.result}</div>
+//     </li>
+//   );
+// });
+// Try.displayName = "Try";
+
+// export default Try;
--- React-game/section3/TryClass.jsx
+++ React-game/section3/TryClass.jsx
@@ -12,4 +12,28 @@ class TryClass extends PureComponent {
   }
 }
 
-export default TryClass;
+// export default TryClass;
+
+// import React, { PureComponent } from "react";
+
+// class TryClass extends PureComponent {
+//   constructor(props) {
+//     super(props);
+//     const filtered = this.props.filtered(() => {});
+//     state = {
+//       result: this.props.result,
+//       try: this.props.try,
+//     };
+//   }
+//   render() {
+//     const { tryInfo } = this.props;
+//     return (
+//       <li>
+//         <div>{tryInfo.try}</div>
+//         <div>{tryInfo.result}</div>
+//       </li>
+//     );
+//   }
+// }
+
+// export default TryClass;
