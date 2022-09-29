a5db0df0e04f407eeb085c0d52873c7cc96cf8bd
0131ryu <131ryuji@gmail.com>
2022-09-29 15:27:54 +0900

    로또생성기 - react

---
+++ React-game/section6/LottoClass.jsx
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+import Ball from "./Ball";
+
+function getWinNumbers() {
+  console.log("getWinNumbers");
+  const candidate = Array(45)
+    .fill()
+    .map((v, i) => i + 1);
+  const shuffle = [];
+  while (candidate.length > 0) {
+    shuffle.push(
+      candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]
+    );
+  }
+  const bonusNumber = shuffle[shuffle.length - 1];
+  const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+  return [...winNumbers, bonusNumber];
+}
+
+class LottoClass extends Component {
+  state = {
+    winNumbers: getWinNumbers(), // 당첨 숫자들
+    winBalls: [],
+    bonus: null, // 보너스 공
+    redo: false,
+  };
+
+  timeouts = [];
+
+  runtimeouts = () => {
+    console.log("runtimeouts");
+    const { winNumbers } = this.state;
+    for (let i = 0; i < winNumbers.length - 1; i++) {
+      this.timeouts[i] = setTimeout(() => {
+        this.setState((prevState) => {
+          return {
+            winBalls: [...prevState.winBalls, winNumbers[i]],
+          };
+        });
+      }, (i + 1) * 1000);
+    }
+    this.timeouts[6] = setTimeout(() => {
+      this.setState({
+        bonus: winNumbers[6],
+        redo: true,
+      });
+    }, 7000);
+  };
+
+  componentDidMount() {
+    console.log("didMount");
+    this.runtimeouts();
+  }
+
+  //  onClickRedo 누를 때 setTimeout 다시 실행해야 함
+  componentDidUpdate(prevProps, prevState) {
+    console.log("didUpdate");
+    //이 조건문이 중요(redo를 눌렀을때만, 조건문이 없을 경우 매 순간 runTimouts이 실행됨)
+    if (this.state.winBalls.length === 0) {
+      this.runtimeouts();
+    }
+  }
+
+  //setTimeout, setInterval 정리(메모리 누수 발생 방지)
+  componentWillUnmount() {
+    this.timeouts.forEach((v) => {
+      clearTimeout(v);
+    });
+  }
+
+  onClickRedo = () => {
+    console.log("onClickRedo");
+    this.setState({
+      winNumbers: getWinNumbers(), // 당첨 숫자들
+      winBalls: [],
+      bonus: null, // 보너스 공
+      redo: false,
+    });
+    this.timeouts = [];
+  };
+
+  render() {
+    const { winBalls, bonus, redo } = this.state;
+    return (
+      <>
+        <div>당첨 숫자</div>
+        <div id="결과창">
+          {winBalls.map((v) => (
+            <Ball key={v} number={v} />
+          ))}
+        </div>
+        <div>보너스!</div>
+        {bonus && <Ball number={bonus} />}
+        {redo && <button onClick={this.onClickRedo}>한 번 더!</button>}
+      </>
+    );
+  }
+}
+
+export default LottoClass;
---
+++ React-game/section6/client.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+// import LottoClass from "./LottoClass";
+import Lotto from "./Lotto";
+
+createRoot(document.getElementById("root")).render(<Lotto />);
