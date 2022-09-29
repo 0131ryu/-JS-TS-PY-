707a619ed83513ba9a8e51e908aa41e3667574e1
0131ryu <131ryuji@gmail.com>
2022-09-29 22:59:54 +0900

    틱택토 성능 최적화 - memo 사용

--- React-game/section7/Table.jsx
+++ React-game/section7/Table.jsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { memo } from "react";
 import Tr from "./Tr";
 
-const Table = ({ tableData, dispatch }) => {
+const Table = memo(({ tableData, dispatch }) => {
   return (
     //i가 몇 번째 줄인지 나타냄
     <table>
       {Array(tableData.length)
         .fill()
         .map((tr, i) => (
-          <Tr dispatch={dispatch} rowIndex={i} rowData={tableData[i]} />
+          <Tr key={i} dispatch={dispatch} rowIndex={i} rowData={tableData[i]} />
         ))}
     </table>
   );
-};
+});
 
 export default Table;
--- React-game/section7/Tr.js
+++ React-game/section7/Tr.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useRef, useEffect, memo } from "react";
 import Td from "./Td";
 
-const Tr = ({ rowData, rowIndex, dispatch }) => {
+const Tr = memo(({ rowData, rowIndex, dispatch }) => {
   console.log("tr rendered");
+  const ref = useRef([]);
+  useEffect(() => {
+    console.log(
+      rowData === ref.current[0],
+      rowIndex === ref.current[1],
+      dispatch === ref.current[2]
+    );
+    ref.current = [rowData, rowIndex, dispatch];
+  }, [rowData, rowIndex, dispatch]); //모든 props 다 넣기
   return (
     <tr>
       {Array(rowData.length)
         .fill()
         .map((td, i) => (
           <Td
+            key={i}
             cellIndex={i}
             rowIndex={rowIndex}
             cellData={rowData[i]}
@@ -19,6 +29,6 @@ const Tr = ({ rowData, rowIndex, dispatch }) => {
         ))}
     </tr>
   );
-};
+});
 
 export default Tr;
