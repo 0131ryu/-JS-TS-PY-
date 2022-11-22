bca05a8868ba0fb35bdb9f7da7d42514c920d57e
0131ryu <131ryuji@gmail.com>
2022-11-22 23:38:51 +0900

    배열, 리스트 구간합 문제

---
+++ Python/CodingTest/03/test.py
@@ -0,0 +1,18 @@
+a = 3
+b = 4
+
+
+def hap(a1, b1):
+    c = a1 + b1
+    return c
+
+
+ret = hap(a, b)
+print(ret)
+
+# 단축키
+# shift + F5 : 디버깅 모드 종료
+# F5 : 디버깅 실행
+# F9 : break point(빨강색)
+# F10 : 인터프리터 한 줄 이동
+# F11 : 함수 안으로 디버깅
